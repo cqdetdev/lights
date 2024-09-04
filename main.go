b0d6eda5ddf83d3446423fdce1403280bca95ebc
cqdetdev <101936396+cqdetdev@users.noreply.github.com>
2024-09-03 20:39:06 -0400

    feat: fade animation

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ const UPDATE_INTERVAL = 15 * time.Millisecond
 // Should be different for everybody, configure this accordingly.
 const DEVICE_NAME = "KS03~8a0035"
 
+const FADE_STEPS = 2
+
 func main() {
 	adapter := bluetooth.DefaultAdapter
 	err := adapter.Enable()
@@ -73,15 +75,45 @@ func main() {
 		return
 	}
 
+	var lastColor []int
+	var fading bool
+
 	for {
+		if fading {
+			continue
+		}
 		bounds := screenshot.GetDisplayBounds(0)
 		// TODO: Make this configurable (i.e multiple lights, top bottom resolution)
 		img, _ := screenshot.CaptureRect(bounds)
-		cc := adjustColorForOrange(averageImageColor(img))
+		cc := (averageImageColor(img))
 
-		go color.WriteWithoutResponse([]byte(rgb(cc[0], cc[1], cc[2], 0, 50)))
+		if len(lastColor) == 0 {
+			lastColor = cc
+		}
+
+		if lastColor[0] == cc[0] && lastColor[1] == cc[1] && lastColor[2] == cc[2] {
+			color.WriteWithoutResponse([]byte(rgb(cc[0], cc[1], cc[2], 0, 50)))
+			continue
+		}
+
+		for i := 0; i <= FADE_STEPS; i++ {
+			intermediateColor := []int{
+				lastColor[0] + (cc[0]-lastColor[0])*i/FADE_STEPS,
+				lastColor[1] + (cc[1]-lastColor[1])*i/FADE_STEPS,
+				lastColor[2] + (cc[2]-lastColor[2])*i/FADE_STEPS,
+			}
+
+			color.WriteWithoutResponse([]byte(rgb(intermediateColor[0], intermediateColor[1], intermediateColor[2], 0, 50)))
+
+			time.Sleep(UPDATE_INTERVAL)
+		}
+
+		fading = false
+
+		lastColor = cc
+
 		//fmt.Printf("Color updated to RGB(%d, %d, %d)\n", cc[0], cc[1], cc[2])
 
-		time.Sleep(15 * time.Millisecond)
+		time.Sleep(UPDATE_INTERVAL)
 	}
 }
